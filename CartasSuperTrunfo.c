3c91c1ca16fa8540b4fda7b0a29705d3a2620294
IgorHFerreiraC <igor.castro.1@hotmail.com>
2025-02-28 01:20:31 +0000

    Base da primeira carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,6 +17,43 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    
+    /*
+    %d: Imprime um inteiro no formato decimal.
+    %i: equivalente a %d.
+    %f: Imprime um número de pontos flutuantes no formato padrão.
+    %e: Imprime um número de ponto flutuante na notação cientifica.
+    %c: Imprime um único caractere.
+    %s: Imprime uma cadeia(strings) de caracteres.
+    */
 
-    return 0;
+    char Estado[50];
+    char Código_da_Carta[50];
+    char Nome_da_cidade[50];
+    int população;    
+    float área;
+    float PIB;
+    int pontos_turisticos;
+
+printf("Digite o nome do Estado: \n");
+scanf("%s", Estado);
+
+printf("Digite o Codigo da carta: \n");
+scanf("%s", Código_da_Carta);
+
+printf("Digite o nome da cidade: \n");
+scanf("%s", Nome_da_cidade);
+
+printf("Digite a população: \n");
+scanf("%d", &população);
+
+printf("Digite a area: \n");
+scanf("%f", &área);
+
+printf("Digite o PIB: \n");
+scanf("%f", &PIB);
+
+printf("Digite os pontos turisticos: \n");
+scanf("%d", &pontos_turisticos);
+
 }
