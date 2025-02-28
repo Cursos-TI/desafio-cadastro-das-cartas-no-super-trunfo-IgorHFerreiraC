1be15fdc2e1db9f54a8a972e17161413ab061ae1
IgorHFerreiraC <igor.castro.1@hotmail.com>
2025-02-28 01:34:55 +0000

    versão sem acento

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,9 +5,7 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
-
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
+// Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
     // Cadastro das Cartas:
@@ -17,7 +15,7 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-    
+
     /*
     %d: Imprime um inteiro no formato decimal.
     %i: equivalente a %d.
@@ -26,12 +24,13 @@ int main() {
     %c: Imprime um único caractere.
     %s: Imprime uma cadeia(strings) de caracteres.
     */
+int main() {
 
     char Estado[50];
-    char Código_da_Carta[50];
+    char Codigo_da_Carta[50];
     char Nome_da_cidade[50];
-    int população;    
-    float área;
+    int populacao;    
+    float area;
     float PIB;
     int pontos_turisticos;
 
@@ -39,16 +38,16 @@ printf("Digite o nome do Estado: \n");
 scanf("%s", Estado);
 
 printf("Digite o Codigo da carta: \n");
-scanf("%s", Código_da_Carta);
+scanf("%s", Codigo_da_Carta);
 
 printf("Digite o nome da cidade: \n");
 scanf("%s", Nome_da_cidade);
 
 printf("Digite a população: \n");
-scanf("%d", &população);
+scanf("%d", &populacao);
 
 printf("Digite a area: \n");
-scanf("%f", &área);
+scanf("%f", &area);
 
 printf("Digite o PIB: \n");
 scanf("%f", &PIB);
